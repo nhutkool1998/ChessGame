6417c1142a4188e383ff2e6758a6c653defdce09
Nguyen Minh Nhut <33351987+nhutkool1998@users.noreply.github.com>
2019-05-14 19:59:24 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,10 +24,13 @@
 
 
 var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    mapNode:null, 
-    tileMap: null, 
-    ctor:function () {
+    sprite: null,
+    mapNode: null,
+    tileMap: null,
+    tileWidth: null,
+    tileHeight: null,
+    chessScale: null,
+    ctor: function () {
         //////////////////////////////
         // 1. super init first
         this._super();
@@ -36,19 +39,34 @@ var HelloWorldLayer = cc.Layer.extend({
         // 2. add a menu item with "X" image, which is clicked to quit the program
         //    you may modify it.
         // ask the window size
-      //  this.tileMap  = cc.Sprite.create("res/newMappp.png");
+        //  this.tileMap  = cc.Sprite.create("res/newMappp.png");
         this.tileMap = cc.TMXTiledMap.create(res.chessMap);
         cc.log(this.tileMap);
-        this.tileMap.setPosition(cc.winSize.width/2,cc.winSize.height/2);
-        this.tileMap.setAnchorPoint(0.5,0.5);
-        this.addChild(this.tileMap,100,100);
-        
+        this.tileMap.setPosition(cc.winSize.width / 2, cc.winSize.height / 2);
+        this.tileMap.setAnchorPoint(0.5, 0.5);
+        this.addChild(this.tileMap, 100, 100);
+        var kingSprite = cc.Sprite(res.blackKing);
+        this.tileWidth = this.tileMap.getTileSize().width;
+        this.tileHeight = this.tileMap.getTileSize().height;
+        this.chessScale = this.tileWidth / kingSprite.getContentSize().width;
+        cc.setScale(this.chessScale);
+        var position = this.calculatePosition(0, 0);
+        kingSprite.setPosition(position);
         return true;
-    }
+    },
+
+    calculatePosition: function (x, y) {
+        var topLeftX = this.tileMap.x + this.tileMap.width * this.tileWidth / 2;
+        var topLeftY = this.tileMap.y + this.tileMap.height * this.tileHeight / 2;
+        var resX = topLeftX + x * this.tileWidth;
+        var resY = topLeftY + y * this.tileWidth;
+        cc.log("topLeftY",topLeftY);
+        return cc.p(resX, resY);
+    },
 });
 
 var HelloWorldScene = cc.Scene.extend({
-    onEnter:function () {
+    onEnter: function () {
         this._super();
         var layer = new HelloWorldLayer();
         this.addChild(layer);
